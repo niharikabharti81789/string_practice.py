e7fa01a188f1c563a6914747d8bfda96c0c4436e
niharika_codes <niharikabharti240@gmail.com>
2026-05-07 12:01:13 +0530

    Added Python string methods and practice questions
    Python beginner practice programs covering string methods and user input.

---
+++ python_strings.py
@@ -0,0 +1,25 @@
+text =  "apna college"
+ch= text[6]
+print(ch)
+text="i am niharika bharti"
+print(text.capitalize())
+
+text = text.capitalize()
+print(text)
+
+text="i am niharika bharti"
+print(text.replace("a","o"))
+
+
+text="i am niharika bharti"
+print(text.find("a"))
+
+# practice question1...............................................
+
+name = input ("enter your name")
+print("length of your name is :", len(name))
+
+# practice question2...............................................
+
+text = "hii im the $ symbol $99.99"
+print (text.count("$"))
